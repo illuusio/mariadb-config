0a4cfecdcb3ee42ba1992b59495af7bebce9c779
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2024-09-30 11:10:19 +0300

    Add some perfomance tuning options
    Add some performance tuning options for modern SSD and servers
    which have enough memory.
    
     * local_infile = OFF
     * max_allowed_packet = 512M
     * max_connections = 256
     * max_heap_table_size = 256M
     * performance_schema = ON
     * query_cache_limit = 128M
     * query_cache_size = 10M
     * table_open_cache = 4096
     * tmp_table_size = 256M

---
+++ test/system-variables.sql
@@ -0,0 +1,10 @@
+# Show system variables which we have altered
+SHOW VARIABLES LIKE 'local_infile';
+SHOW VARIABLES LIKE 'max_allowed_packet';
+SHOW VARIABLES LIKE 'max_connections';
+SHOW VARIABLES LIKE 'max_heap_table_size';
+SHOW VARIABLES LIKE 'performance_schema';
+SHOW VARIABLES LIKE 'query_cache_limit';
+SHOW VARIABLES LIKE 'query_cache_size';
+SHOW VARIABLES LIKE 'table_open_cache';
+SHOW VARIABLES LIKE 'tmp_table_size';
