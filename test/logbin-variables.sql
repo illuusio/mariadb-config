a76d8463eb431e3598f23ad6950c65e38570e2a0
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2024-09-30 13:03:03 +0300

    Add basic replication config
    Basic replication config which includes:
     * server_id = 1
     * log-basename = mariadb-server-base
     * binlog_format = MIXED
     * expire_logs_days = 7
     * log_bin_compress = ON
     * log_bin = ON
     * log_bin_trust_function_creators = ON

---
+++ test/logbin-variables.sql
@@ -0,0 +1,8 @@
+# Show logging and replication variables which we have altered
+SHOW VARIABLES LIKE 'server_id';
+SHOW VARIABLES LIKE 'log_bin_basename';
+SHOW VARIABLES LIKE 'binlog_format';
+SHOW VARIABLES LIKE 'expire_logs_days';
+SHOW VARIABLES LIKE 'log_bin_compress';
+SHOW VARIABLES LIKE 'log_bin';
+SHOW VARIABLES LIKE 'log_bin_trust_function_creators';
